63c598d5bb4537321e81751f325672f3b7d76075
piekarskipiotr <ppiekarski8@gmail.com>
2024-01-16 18:49:05 +0100

    Update IconButtons shape and add padding

--- app/src/main/java/com/piekarskipiotr/remindmehere/presentation/home/ListFragment.kt
+++ app/src/main/java/com/piekarskipiotr/remindmehere/presentation/home/ListFragment.kt
@@ -1,13 +1,18 @@
 package com.piekarskipiotr.remindmehere.presentation.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.piekarskipiotr.remindmehere.R
 
@@ -16,7 +21,10 @@ fun ListFragment(navController: NavController) {
     Box(modifier = Modifier.fillMaxSize()) {
         IconButton(
             onClick = { navController.navigate("mapFragment") },
-            modifier = Modifier.align(Alignment.TopEnd)
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(16.dp)
+                .background(Color.White, shape = CircleShape)
         ) {
             Icon(
                 painterResource(id = R.drawable.map_icon),
--- app/src/main/java/com/piekarskipiotr/remindmehere/presentation/home/MapFragment.kt
+++ app/src/main/java/com/piekarskipiotr/remindmehere/presentation/home/MapFragment.kt
@@ -3,6 +3,8 @@ package com.piekarskipiotr.remindmehere.presentation.home
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.List
 import androidx.compose.material3.Icon
@@ -11,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -43,7 +46,10 @@ fun MapFragment(navController: NavController) {
 
         IconButton(
             onClick = { navController.navigate("listFragment") },
-            modifier = Modifier.align(Alignment.TopEnd).background(Color.White)
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(16.dp)
+                .background(Color.White, shape = CircleShape)
         ) {
             Icon(Icons.Rounded.List, contentDescription = "List view of reminders")
         }
