40c90c9da668ba3263f00568eecb5098ac9cf6ce
piekarskipiotr <ppiekarski8@gmail.com>
2024-01-16 19:08:50 +0100

    Request location permissions on app start

--- app/src/main/java/com/piekarskipiotr/remindmehere/MainActivity.kt
+++ app/src/main/java/com/piekarskipiotr/remindmehere/MainActivity.kt
@@ -1,16 +1,42 @@
 package com.piekarskipiotr.remindmehere
 
+import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.piekarskipiotr.remindmehere.presentation.home.HomeActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var locationPermissionRequest: ActivityResultLauncher<Array<String>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, HomeActivity::class.java))
-        finish()
+        locationPermissionRequest =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                val granted = permissions.entries.all { it.value }
+                if (granted) {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    finish()
+                } else {
+                    finish()
+                }
+            }
+
+        requestLocationPermissions()
+
+
+    }
+
+    private fun requestLocationPermissions() {
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            )
+        )
     }
 }
